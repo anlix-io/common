ab022c57007bc9b767f1f01c96016d9a976db0aa
jotape <jotape@protonmail.com>
2022-03-18 03:38:07 -0300

    add better gitignore

--- lib/file.ts
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-
-export const exportJSONToFile = (json: Object | string) => {
-  let jsonFile = '';
-  if (typeof json === 'object') {
-    jsonFile = JSON.stringify(json);
-  }
-  fs.writeFileSync('jsonFile.json', jsonFile);
-}
